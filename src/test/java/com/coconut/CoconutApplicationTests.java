7039803660d3b16c9f053497866600ffd81dfa15
hjoon <guswns3371@gmail.com>
2023-05-01 14:46:26 +0900

    chore: build.gradle 수정
    - jdk 17
    - spring boot version 2.7.11

--- src/test/java/com/coconut/CoconutApplicationTests.java
+++ src/test/java/com/coconut/CoconutApplicationTests.java
@@ -1,26 +1,4 @@
 package com.coconut;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.GsonBuilder;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-
-@SpringBootTest
 class CoconutApplicationTests {
-
-    @Test
-    void contextLoads() {
-        String oldReadMembersString = "[15, 3, 4]";
-
-        ArrayList<String> oldReadMembers = new GsonBuilder().create().fromJson(oldReadMembersString, new TypeToken<ArrayList<String>>(){}.getType());
-        oldReadMembers.add("3");
-        oldReadMembers = new ArrayList<String>(new HashSet<String>(oldReadMembers));
-        Collections.sort(oldReadMembers);
-        System.out.println(oldReadMembers.toString());
-    }
-
 }
